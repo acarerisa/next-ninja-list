3e999d8dab4e8cd6f51fd83bed48739738045228
acarerisa <acarerisa84@gmail.com>
2021-05-20 15:29:59 +0300

    second commit

--- pages/about.js
+++ pages/about.js
@@ -4,11 +4,11 @@ const About = () => {
     return ( 
         <>
         <Head>
-        <title>Ninja List | About</title>
+        <title>Ninja List | Hakkımda</title>
           <meta name="keywords" content="ninjas" />
         </Head>
         <div>
-            <h1>About</h1>
+            <h1>Hakkımda</h1>
             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto error officia quaerat atque eveniet maiores, odio nobis repudiandae commodi assumenda facere. Aliquam, in. Sint dicta eaque in enim officiis pariatur.</p>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, voluptas rem expedita enim autem eos necessitatibus laborum amet blanditiis, omnis sit quod quasi? Voluptates accusantium repellendus expedita magnam libero distinctio.</p>
         </div>
